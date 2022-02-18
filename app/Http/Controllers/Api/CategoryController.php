f6c2c192388b437c9d0a7c17f19953038a77c268
Samuel Gomes <hellish.doom@gmail.com>
2022-02-18 18:32:05 -0300

    Implemntado métodos index, show e store

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
@@ -18,9 +19,13 @@ class CategoryController extends Controller
         return $category;
     }
 
-    public function store()
+    public function store(Request $request)
     {
-
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'is_active' => 'boolean'
+        ]);
+        return Category::create($request->all());
     }
 }
 
